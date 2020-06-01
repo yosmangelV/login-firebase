6ee480eecb461ce7b83edca85a01ced18b548945
Yosmangel <yadamesp@everis.com>
2020-06-01 17:39:57 +0200

    Clase completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -18,6 +18,8 @@ import { LoginComponent } from './pages/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {UsuarioModel} from '../../../models/usuario.model';
+import {NgForm} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -6,11 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  usuario: UsuarioModel;
+  recordarme = false;
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+  }
 
-  constructor() { }
-
-  ngOnInit() { }
-
+  onSubmit( form: NgForm) {
+    if ( form.invalid ) {
+      return;
+    }
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+    this.auth.nuevoUsuario(this.usuario).subscribe( data => {
+      console.log(data);
+      Swal.close();
+      if (this.recordarme) {
+        localStorage.setItem('email', this.usuario.email);
+      }
+      this.router.navigateByUrl('/home');
+    }, (err) => {
+      console.log(err.error.error.message);
+      Swal.fire({
+        type: 'error',
+        title: 'Error al registrar',
+        text: err.error.error.message
+      });
+    });
+  }
 
 }
